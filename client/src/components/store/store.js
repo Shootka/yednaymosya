9ea8be11169b0f9a5a1decd715ac686bc03742b8
shootka <pirathaker315@gmail.com>
2022-05-08 14:04:12 +0300

    update store

--- client/src/components/store/store.js
+++ client/src/components/store/store.js
@@ -1,4 +1,25 @@
-import {createStore} from "redux";
+const {createStore} = require("redux");
+
+const initialState = {
+  news:{
+    data: [],
+    isLoading: true,
+    error: null
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_NEWS":{
+      return {
+        ...state, news: {data: [...action.payload], isLoading: false}
+      };
+    }
+    default:
+      return state
+  }
+}
+
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
