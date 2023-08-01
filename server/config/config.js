deb1fbf0701f87768d9fb3aa73bdb6f30a6d6b2b
KostyaBardok <105067504+KostyaBardok@users.noreply.github.com>
2023-08-01 20:09:18 +0300

    :)

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 let config = {
-    MONGO_URI: 'mongodb+srv://KostyaBardok:zoj-2000@trainingcluster.it0vs.mongodb.net/Test?retryWrites=true&w=majority'
+    MONGO_URI: 'mongodb+srv://name:password@trainingcluster.it0vs.mongodb.net/Test?retryWrites=true&w=majority'
 }
 
 export default config
